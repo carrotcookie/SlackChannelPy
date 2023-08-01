5448037e6ffe579c84a41828df1f56393f4e1f91
Youbin Park <a44121078@gmail.com>
2023-08-01 15:08:32 +0900

    슬랙 이벤트 발생시 프론트에도 알려줄 수 있도록 sse 도입

--- app.py
+++ app.py
@@ -6,10 +6,15 @@ import json
 from mongo import delete_mongo
 from embedding import ingest_channel, delete_chroma, check_db
 from util import messages_to_csv, delete_channel_csv, unix_to_utc
+from datetime import datetime
+import time
+from collections import deque
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 CORS(app)
+
+SLACK_EVENTS = deque()
 
 @app.route("/api/uploadChannel", methods = ["POST"])
 def upload_slack_channel():
@@ -71,7 +76,7 @@ def chat_in_channel():
 def check_collection():
   # 디버깅
   print("check_collection 실행 ---------------------------------------")
-
+  
   # 전달받은 정보
   user_id = request.json.get("userId")
   channel_id = request.json.get("channelId")
@@ -109,11 +114,35 @@ def message_handler(event):
       "text": event["text"],
       "ts": unix_to_utc(event["ts"]),
     }
-    print(dto)
+    SLACK_EVENTS.append(dto)
+
     return dto
 
     return make_response("ok", 200, )
 
+@app.route("/stream")
+def stream():
+
+  def get_data():
+    while True:
+      if SLACK_EVENTS:
+        dto = json.dumps(SLACK_EVENTS.popleft()).encode("utf-8")
+        yield dto
+
+  return Response(get_data(), mimetype = "text/event-stream")
+
+# @app.route("/test", methods = ["POST"])
+# def append_message():
+#   dto = {
+#       "client_msg_id": event["client_msg_id"],
+#       "user_id": event["user"],
+#       "user_name": get_user_name(event["user"]),
+#       "text": event["text"],
+#       "ts": unix_to_utc(event["ts"]),
+#     }
+#   user_id = request.json.get("userId")
+#   channel_id = request.json.get("channelId")
+#   question = request.json.get("question")
   
 # def update_slack_channel(message):
 #   dto = {
